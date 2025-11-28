8958ac531a05fc9a10e4ddb5fc7e2b5f138e497e
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2025-11-28 12:31:40 +0500

    Update main.rs

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Implement me!");
+
+    fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+        let mut largest = list[0];
+
+        for &item in list {
+            // This comparison works because we constrained T to require the 'PartialOrd' trait
+            if item > largest {
+                largest = item;
+            }
+        }
+
+        largest
+    }
+
+    // This works for integers:
+    let number_list = vec![34, 50, 25, 100, 65];
+    let f = &number_list ;
+    let result = largest(&number_list); // T becomes i32
+
+    // And it works for characters:
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list); // T becomes char
+
 }
