3fb43b90edb115987c9ab211985ffb82205d785f
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2025-12-31 04:53:10 +0500

    Rust by Example

--- rust_by_examples/tests/integration_test.rs
+++ rust_by_examples/tests/integration_test.rs
@@ -2,5 +2,5 @@
 // Cargo looks for integration tests in tests directory next to src.
 #[test]
 fn integration_test_add() {
-    assert_eq!(step_1_1::my_doc_test_add(2, 3), 5) ;
+    assert_eq!(rust_by_examples::my_doc_test_add(2, 3), 5) ;
 }
