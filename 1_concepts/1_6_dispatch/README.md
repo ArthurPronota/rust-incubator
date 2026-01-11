cfa39877a682c97a4fa2ef80aa4e56a3421dd5d2
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-01-11 17:47:41 +0500

    1.6

--- 1_concepts/1_6_dispatch/README.md
+++ 1_concepts/1_6_dispatch/README.md
@@ -157,7 +157,6 @@ There is a handy [momo] crate, which generates this boilerplate automatically in
 
 ## Task
 
-Given the following `Storage` abstraction and `User` entity:
 Дайте следующую `Storage` абстракцию и `User` элемент:
 ```rust
 trait Storage<K, V> {
@@ -172,8 +171,6 @@ struct User {
     activated: bool,
 }
 ```
-
-Implement `UserRepository` type with injectable `Storage` implementation, which can get, add, update and remove `User` in the injected `Storage`. Make two different implementations: one should use [dynamic dispatch][2] for `Storage` injecting, and the other one should use [static dispatch][1].
 
 Реализуйте тип `UserRepository` с внедряемой реализацией `Storage`, которая может получать, добавлять, обновлять и удалять `User` во внедренном `Storage`. Создайте две разные реализации: одна должна использовать [динамическую диспетчеризацию][2] для внедрения `Storage`, а другая — [статическую диспетчеризацию][1].
 
--- 1_concepts/1_6_dispatch/src/main.rs
+++ 1_concepts/1_6_dispatch/src/main.rs
@@ -57,8 +57,9 @@ fn main() {
             // Он сначала ищет метод get среди "родных" методов HashMap.
             // Находит стандартный метод HashMap::get.
             // Использует его.
+            // так:
             //self.get(key)
-            // или так
+            // или так:
             HashMap::get(&self, key)
         }
 
@@ -72,9 +73,10 @@ fn main() {
             // Когда компилятор видит self.remove(key):
             // Он сначала ищет метод get среди "родных" методов HashMap.
             // Находит стандартный метод HashMap::remove.
-            // Использует его.        
+            // Использует его.
+            // так:
             // self.remove(key)
-            // или так
+            // или так:
             HashMap::remove(self, key)
         }
 
@@ -82,23 +84,110 @@ fn main() {
 
     let mut repo = UserRepository::new(HashMap::new());
 
+    let id_user = 42u64 ;
+
     let user = User {
-        id: 42,
+        id: id_user,
         email: Cow::Borrowed("m@n.c"),
         activated: true,
     };    
 
     repo.set(user.id, user);
 
-    println!("{:?}", repo.get(42)) ;
+    println!("{:?}", repo.get(id_user)) ;
 
-    repo.remove(&42) ;
+    repo.remove(&id_user) ;
 
-    println!("{:?}", repo.get(42)) ;
+    println!("{:?}", repo.get(id_user)) ;
     }
 
     // Динамическая диспетчеризация
     {
+        use std::borrow::Cow;
+        use std::collections::HashMap;
 
+        trait Storage<K, V> {
+            fn set(&mut self, key: K, val: V);
+            fn get(&self, key: &K) ->Option<&V>;
+            fn remove(&mut self, key: &K) ->Option<V>;
+        }
+
+        #[derive(Debug)]
+        struct User {
+            id: u64,
+            email: Cow<'static, str>,
+            activated: bool,
+        }        
+
+        struct UserRepository {
+            storage:    Box<dyn Storage<u64, User>>
+        }
+
+        impl UserRepository {
+
+            fn new(storage: Box<dyn Storage<u64, User>>) ->Self {
+                Self { storage }
+            }
+
+            fn set(&mut self, key: u64, val: User) {
+                self
+                    .storage
+                    .set(key, val)
+                    ;
+            }
+
+            fn get(&self, key: &u64) ->Option<&User> {
+                self
+                    .storage
+                    .get(key)
+            }
+
+            fn remove(&mut self, key: &u64) ->Option<User> {
+                self
+                    .storage
+                    .remove(key)
+            }
+
+        }
+
+        impl<K, V> Storage<K, V> for HashMap<K, V> 
+            where K: std::hash::Hash + Eq
+        {
+            fn get(&self, key: &K) -> Option<&V> {
+                // так:
+                // self.get(key)
+                // или так:
+                HashMap::get(&self, key)
+            }
+
+            fn remove(&mut self, key: &K) -> Option<V> {
+                // так:
+                // self.remove(key)
+                // или так:
+                HashMap::remove(self, key)
+            }
+
+            fn set(&mut self, key: K, val: V) {
+                self.insert(key, val) ;
+            }
+        }
+
+        let mut repo = UserRepository::new(Box::new(HashMap::new())) ;
+
+        let id_user = 42u64 ;
+
+        let user = User {
+            id:     id_user,
+            email: Cow::Borrowed("m@m.c"),
+            activated:  true
+        } ;
+
+        repo.set(user.id, user) ;
+
+        println!("{:?}", repo.get(&id_user)) ;
+
+        repo.remove(&id_user) ;
+
+        println!("{:?}", repo.get(&id_user)) ;
     }
 }
