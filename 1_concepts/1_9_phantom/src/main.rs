d89080819a083eb078f2935c17e7c6ecf9dc8cff
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-01-14 11:07:22 +0500

    1.9

--- 1_concepts/1_9_phantom/src/main.rs
+++ 1_concepts/1_9_phantom/src/main.rs
@@ -1,3 +1,10 @@
+// пример укорачивания времени жизни для y
+fn implicit_lifetime<'a, 'b>(x: &'a i32, y: &'b i32) ->&'a i32 
+    where 'b: 'a,   // 'b живёт не меньше чем 'a
+{
+    y
+}
+
 fn main() {
     println!("Implement me!");
 }
