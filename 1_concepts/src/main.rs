ef2e06339e28bdbbf1753246ad6bb8606c7fd5e6
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-01-15 12:36:23 +0500

    1

--- 1_concepts/src/main.rs
+++ 1_concepts/src/main.rs
@@ -1,4 +1,11 @@
 use std::borrow::Cow ;
+use std::marker::PhantomData ;
+
+struct MyPtr<'a, T> {
+    ptr: *const u8, // Компилятор выдаст ошибку: 'a и T не используются
+    _marker: PhantomData<&'a T>,
+}
+
 fn main() {
     println!("Implement me!");
 }
