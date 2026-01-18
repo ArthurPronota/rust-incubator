de4ecea0360a5fdbeb0447f4b6c7b7feb3c2ff86
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-01-18 18:03:27 +0500

    2.1

--- 2_idioms/2_1_type_safety/src/main.rs
+++ 2_idioms/2_1_type_safety/src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Clone)]
+struct Post {
+    id: u64,
+    user_id: u64,
+    title: String,
+    body: String,
+}
+
+fn repost(post: &Post, new_author_id: u64) -> Post {
+    let mut new_post = post.clone();
+    new_post.id = new_author_id;  // Oops!
+    new_post
+}
+
 fn main() {
     println!("Implement me!");
 }
