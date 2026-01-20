8abaa04054fa16528aaa78c954be80840aa665c9
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-01-20 06:42:38 +0500

    2.2

--- 2_idioms/2_2_mem_replace/src/main.rs
+++ 2_idioms/2_2_mem_replace/src/main.rs
@@ -1,3 +1,62 @@
+
+#[derive(Clone, Debug, PartialEq)]
+struct Trinity<T> {
+    a: T,
+    b: T,
+    c: T,
+}
+
+impl<T: Clone> Trinity<T> {
+    fn rotate(&mut self) {
+        let a = self.a.clone();
+        let b = self.b.clone();
+        let c = self.c.clone();
+        self.a = b;
+        self.b = c;
+        self.c = a;
+    }
+}
+
+#[derive(Debug)]
+struct Solver<T> {
+    expected: Trinity<T>,
+    unsolved: Vec<Trinity<T>>,
+}
+
+impl<T: Clone + PartialEq> Solver<T> {
+    fn resolve(&mut self) {
+        let mut unsolved = Vec::with_capacity(self.unsolved.len());
+        'l: for t in self.unsolved.iter_mut() {
+            for _ in 0..3 {
+                if *t == self.expected {
+                    continue 'l;
+                }
+                t.rotate();
+            }
+            unsolved.push(t.clone())
+        }
+        self.unsolved = unsolved;
+    }
+}
+
+fn main() {
+    let mut s = Solver {
+        expected: Trinity { a: 1, b: 2, c: 3 },
+        unsolved: vec![
+            Trinity { a: 1, b: 2, c: 3 },
+            Trinity { a: 2, b: 1, c: 3 },
+            Trinity { a: 2, b: 3, c: 1 },
+            Trinity { a: 3, b: 1, c: 2 },
+        ],
+    };
+    s.resolve();
+    println!("{:?}", s)
+}
+
+
+
+
+// ---------------------------------
 use std::collections::HashSet ;
 use std::mem ;
 
@@ -7,16 +66,15 @@ struct Names {
 }
 
 impl Names {
-    /*
-    fn _apply_exclusions(&mut self) {
-        self    // первое изменяемле заимствование &mut self
-            .exclusions
-            .drain(..)
-            .for_each(|name| {
-                self.remove_name(&name);    // второе изменяемле заимствование &mut self
-            })
-    }
-     */
+
+    //fn _apply_exclusions(&mut self) {
+    //    self    // первое изменяемле заимствование &mut self
+    //        .exclusions
+    //        .drain(..)
+    //        .for_each(|name| {
+    //            self.remove_name(&name);    // второе изменяемле заимствование &mut self
+    //        })
+    //}
 
     fn __apply_exclusions(&mut self) {
         self
@@ -61,67 +119,11 @@ impl Names {
     }
 }
 
-/*
+
 // Это НЕ СКОМПИЛИРУЕТСЯ
-fn swap_out(r: &mut String) -> String {
-    let val = *r; // Ошибка: попытка перемещения из-под ссылки.
-                  // После этого r указывал бы на невалидную память.
-    *r = String::from("new"); 
-    val
-}
- */
-
-
-fn main() {
-    let mut s = Solver {
-        expected: Trinity { a: 1, b: 2, c: 3 },
-        unsolved: vec![
-            Trinity { a: 1, b: 2, c: 3 },
-            Trinity { a: 2, b: 1, c: 3 },
-            Trinity { a: 2, b: 3, c: 1 },
-            Trinity { a: 3, b: 1, c: 2 },
-        ],
-    };
-    s.resolve();
-    println!("{:?}", s)
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct Trinity<T> {
-    a: T,
-    b: T,
-    c: T,
-}
-
-impl<T: Clone> Trinity<T> {
-    fn rotate(&mut self) {
-        let a = self.a.clone();
-        let b = self.b.clone();
-        let c = self.c.clone();
-        self.a = b;
-        self.b = c;
-        self.c = a;
-    }
-}
-
-#[derive(Debug)]
-struct Solver<T> {
-    expected: Trinity<T>,
-    unsolved: Vec<Trinity<T>>,
-}
-
-impl<T: Clone + PartialEq> Solver<T> {
-    fn resolve(&mut self) {
-        let mut unsolved = Vec::with_capacity(self.unsolved.len());
-        'l: for t in self.unsolved.iter_mut() {
-            for _ in 0..3 {
-                if *t == self.expected {
-                    continue 'l;
-                }
-                t.rotate();
-            }
-            unsolved.push(t.clone())
-        }
-        self.unsolved = unsolved;
-    }
-}
+// fn swap_out(r: &mut String) -> String {
+//    let val = *r; // Ошибка: попытка перемещения из-под ссылки.
+//                  // После этого r указывал бы на невалидную память.
+//    *r = String::from("new"); 
+//    val
+//}
