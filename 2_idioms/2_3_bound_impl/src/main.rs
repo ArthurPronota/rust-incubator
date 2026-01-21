0b2409ab871d3a85c90e71fdf5f391aa99cf7237
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-01-21 13:25:51 +0500

    2.3

--- 2_idioms/2_3_bound_impl/src/main.rs
+++ 2_idioms/2_3_bound_impl/src/main.rs
@@ -1,16 +1,19 @@
 use std::{
     borrow::{Borrow, BorrowMut},
-    num::NonZeroU64, sync::{Arc, Mutex},
+    num::NonZeroU64, // Это позволяет оптимизировать размещение данных в памяти.
+    /* 20260121 Не нужный код
+    sync::{Arc, Mutex},
+    */
 };
 
-/// A projected state built from a series of events.
+/// Прогнозируемое состояние, сформированное на основе ряда событий.
 pub trait Aggregate: Default {
-    /// A static string representing the type of the aggregate.
+    /// Статическая строка, представляющая тип агрегата.
     ///
-    /// Note: This should effectively be a constant value, and should never change.
+    /// Примечание: это значение должно быть постоянным и никогда не должно меняться.
     fn aggregate_type() -> &'static str;
 
-    /// Consumes the event, applying its effects to the aggregate.
+    /// Обрабатывает событие, применяя его последствия к агрегатору.
     fn apply<E>(&mut self, event: E)
     where
         E: AggregateEvent<Self>,
@@ -19,55 +22,91 @@ pub trait Aggregate: Default {
     }
 }
 
-/// An identifier for an aggregate.
+/// Идентификатор агрегата.
 pub trait AggregateId<A>
 where
     A: Aggregate,
 {
-    /// Gets the stringified aggregate identifier.
+    /// Получает строковый идентификатор агрегата.
     fn as_str(&self) -> &str;
 }
 
-/// A thing that happened.
+/// Событие, которое произошло.
 pub trait Event {
-    /// A static description of the event.
+    /// Статическое описание события.
     fn event_type(&self) -> &'static str;
 }
 
-/// An event that can be applied to an aggregate.
+/// Событие, которое может быть применено к агрегату.
 pub trait AggregateEvent<A: Aggregate>: Event {
-    /// Consumes the event, applying its effects to the aggregate.
+    /// Обрабатывает событие, применяя его последствия к агрегатору.
     fn apply_to(self, aggregate: &mut A);
 }
 
-/// Represents an event sequence number, starting at 1
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+/// Представляет собой порядковый номер события, начиная с 1.
+#[derive(
+    Clone,
+    Copy,
+    /* 20260121 Не нужный код
+    Debug,
+    Hash,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord
+     */
+  )
+]
 pub struct EventNumber(NonZeroU64);
 
 impl EventNumber {
-    /// The minimum [EventNumber].
+    /// Минимальное значение [EventNumber].
     #[allow(unsafe_code)]
-    pub const MIN_VALUE: EventNumber =
-        // One is absolutely non-zero, and this is required for this to be
-        // usable in a `const` context.
-        EventNumber(unsafe { NonZeroU64::new_unchecked(1) });
+    pub const MIN_VALUE: EventNumber =  // создание const
+        // Единица абсолютно не равна нулю, и это необходимо для того, 
+        // чтобы это можно было использовать в контексте констант.
+        EventNumber(
+            unsafe {
+                NonZeroU64::new_unchecked(1) // Создает ненулевое значение, не проверяя, является ли оно ненулевым.
+            }
+        );
 
-    /// Increments the event number to the next value.
+    /// Увеличивает номер события до следующего значения.
     #[inline]
     pub fn incr(&mut self) {
-        self.0 = NonZeroU64::new(self.0.get() + 1).unwrap();
+        self.0 = NonZeroU64::new(   // Создает ненулевое значение, если заданное значение не равно нулю.
+                    self.0.get() // Возвращает содержащееся значение как примитивный тип.
+                    +
+                    1
+                )
+                .unwrap()
+                ;
     }
 }
 
-/// An aggregate version.
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+/// Верся агрегата
+#[derive(
+    Clone,
+    Copy,
+    /* 20260121 Не нужный код
+    Debug,
+    Hash,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord
+    */
+  )
+]
 pub enum Version {
-    /// The version of an aggregate that has not had any events applied to it.
+    /// Версия агрегата, к которому еще не были применены никакие события.
     Initial,
     /// The version of the last event applied to the aggregate.
+    /// Версия последнего события, примененная к агрегату.
     Number(EventNumber),
 }
 
+// реализация Default для Version
 impl Default for Version {
     #[inline]
     fn default() -> Self {
@@ -75,20 +114,21 @@ impl Default for Version {
     }
 }
 
+
 impl Version {
-    /// Creates a new `Version` from a number.
+    /// Создает новую версию из заданного числа.
     ///
-    /// The number `0` gets interpreted as being `Version::Initial`, while any other number is interpreted as the
-    /// latest event number applied.
+    /// Число `0` интерпретируется как `Version::Initial`, а любое другое
+    /// число интерпретируется как применен последний номер события.
     #[inline]
     pub fn new(number: u64) -> Self {
-        NonZeroU64::new(number)
-            .map(EventNumber)
-            .map(Version::Number)
-            .unwrap_or(Version::Initial)
+        NonZeroU64::new(number) // Option<NonZeroU64>
+            .map(EventNumber)    // Option<EventNumber(NonZeroU64)>
+            .map(Version::Number)    // Option<Version::Number(EventNumber(NonZeroU64))>
+            .unwrap_or(Version::Initial)    // Version::Number(EventNumber(NonZeroU64)) или Version::Initial
     }
 
-    /// Increments the version number to the next in sequence.
+    /// Увеличивает номер версии до следующей по порядку.
     #[inline]
     pub fn incr(&mut self) {
         match *self {
@@ -98,11 +138,25 @@ impl Version {
     }
 }
 
-/// An aggregate that has been loaded from a source, which keeps track of the version of its last snapshot and the current version of the aggregate.
-#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
+/// Агрегат, загруженный из источника, который отслеживает версию своего 
+/// последнего снимка и текущую версию агрегата.
+#[derive(
+    Clone,
+    Copy,
+    /* 20260121 Не нужный код
+    Debug,
+    Default,
+    Hash,
+    PartialEq,
+    Eq
+    */
+  )
+]
 pub struct HydratedAggregate<A>
+/* 20260121 Не нужный код
 where
     A: Aggregate,
+ */
 {
     version: Version,
     snapshot_version: Option<Version>,
@@ -113,34 +167,34 @@ impl<A> HydratedAggregate<A>
 where
     A: Aggregate,
 {
-    /// The current version of the aggregate.
+    /// Текущая версия агрегата.
     pub fn version(&self) -> Version {
         self.version
     }
 
-    /// The version of the snapshot from which the aggregate was loaded.
+    /// Версия снимка, из которого был загружен агрегат.
     pub fn snapshot_version(&self) -> Option<Version> {
         self.snapshot_version
     }
 
-    /// Updates the snapshot version. Generally used to indicate that a snapshot was taken.
+    /// Обновляет версию снимка. Обычно используется для указания того, что был сделан снимок.
     pub fn set_snapshot_version(&mut self, new_snapshot_version: Version) {
         self.snapshot_version = Some(new_snapshot_version);
     }
 
-    /// The actual aggregate.
+    /// Актуальный агрегат.
     pub fn state(&self) -> &A {
         &self.state
     }
 
-    /// Applies a sequence of events to the internal aggregate.
+    /// Применяет последовательность событий к внутреннему агрегату.
     pub fn apply_events<E: AggregateEvent<A>, I: IntoIterator<Item = E>>(&mut self, events: I) {
         for event in events {
             self.apply(event);
         }
     }
 
-    /// Applies a single event to the aggregate, keeping track of the new aggregate version.
+    /// Применяет одно событие к агрегату, отслеживая новую версию агрегата.
     pub fn apply<E: AggregateEvent<A>>(&mut self, event: E) {
         self.state.apply(event);
         self.version.incr();
@@ -165,12 +219,25 @@ where
     }
 }
 
-/// An identified, specific instance of a hydrated aggregate.
-#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
+/// Идентифицированный, конкретный пример гидратированного агрегата.
+#[derive(
+    Clone,
+    /* 20260121 Не нужный код
+    Copy,
+    Debug,
+    Default,
+    Hash,
+    PartialEq,
+    Eq
+    */
+  )
+]
 pub struct Entity<I, A>
+/* 20260121 Не нужный код
 where
     A: Aggregate,
     I: AggregateId<A>,
+ */
 {
     id: I,
     aggregate: HydratedAggregate<A>,
@@ -181,22 +248,22 @@ where
     A: Aggregate,
     I: AggregateId<A>,
 {
-    /// Creates a new entity from an identifier and an associated hydrated aggregate.
+    /// Создает новый объект на основе идентификатора и связанного с ним гидратированного агрегата.
     pub fn new(id: I, aggregate: HydratedAggregate<A>) -> Self {
         Entity { id, aggregate }
     }
 
-    /// The entity's identifier.
+    /// Идентификатор объекта.
     pub fn id(&self) -> &I {
         &self.id
     }
 
-    /// An immutable reference to the underlying aggregate.
+    /// Неизменяемая ссылка на лежащий в основе агрегат.
     pub fn aggregate(&self) -> &HydratedAggregate<A> {
         &self.aggregate
     }
 
-    /// A mutable reference to the underlying aggregate.
+    /// Изменяемая ссылка на базовый агрегат.
     pub fn aggregate_mut(&mut self) -> &mut HydratedAggregate<A> {
         &mut self.aggregate
     }
@@ -263,5 +330,5 @@ where
 }
 
 fn main() {
-    println!("Refactor me!");
+    println!("The code has been refactored.");
 }
