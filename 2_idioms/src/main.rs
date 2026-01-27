617f2d0a1ac9e885582c51cb30936704ec887d19
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-01-27 11:00:28 +0500

    2

--- 2_idioms/src/main.rs
+++ 2_idioms/src/main.rs
@@ -1,3 +1,63 @@
+//use std::collections::HashMap;
+//use std::fmt;
+//use thiserror::Error; // Для удобных ошибок, можно заменить на ручную реализацию
+
+
+/// перечень допустимых монет
+#[derive(Clone, Copy)]
+enum Coin {
+    One = 1,
+    Two = 2,
+    Five = 5,
+    Ten = 10,
+    Twenty = 20,
+    Fifty = 50,
+}
+
+
+impl Coin {
+
+    fn value(&self) ->u32 {
+        *self as u32    // необъодим traits Clone, Copy
+    }
+
+}
+
+
+
+/*
+// Номиналы монет
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Coin {
+    One = 1,
+    Two = 2,
+    Five = 5,
+    Ten = 10,
+    Twenty = 20,
+    Fifty = 50,
+}
+
+
+impl Coin {
+    // Получить все возможные номиналы для итерации
+    pub fn all() -> [Coin; 6] {
+        [
+            Coin::One,
+            Coin::Two,
+            Coin::Five,
+            Coin::Ten,
+            Coin::Twenty,
+            Coin::Fifty,
+        ]
+    }
+    
+    // Значение монеты
+    pub fn value(&self) -> u32 {
+        *self as u32
+    }
+}
+ */
+
 fn main() {
     println!("Implement me!");
 }
