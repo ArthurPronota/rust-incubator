6b8d4a5c0fc686eff538fa7530d2c258dc0a6696
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-02-03 13:31:43 +0500

    3.4

--- 3_ecosystem/3_4_regex_parsing/src/main.rs
+++ 3_ecosystem/3_4_regex_parsing/src/main.rs
@@ -1,9 +1,102 @@
+// Скомпилированное регулярное выражение для поиска в Unicode стогах.
+use regex::Regex ;
+// Примитив синхронизации, в который номинально можно записать данные только один раз.
+use std::sync::OnceLock ;
+
 fn main() {
-    println!("Implement me!");
+    //println!("Implement me!");
+
+    let res = 
+            parse(
+                //" +10",
+                //" +10$",
+                //" +10.",
+                //" +10.10",
+                // " +10.*",
+                //" +10.15$"
+
+                //" -10"
+                // "abc"
+                "-8",
+            ) ;
+    println!("res: {:?}", res) ;
 }
 
-fn parse(input: &str) -> (Option<Sign>, Option<usize>, Option<Precision>) {
-    unimplemented!()
+/// разбор входной строки `format_spec`
+fn parse(input: &str) -> (
+                            Option<Sign>,       // sign
+                            Option<usize>,      // width
+                            Option<Precision>   // precision 
+                        ) {
+    static RE: OnceLock<Regex> = OnceLock::new() ;
+
+    // Получает содержимое ячейки, инициализируя его значением f(), если ячейка не была инициализирована.
+    let re = RE.get_or_init(|| {
+        Regex::new(
+            r"(?x)      # Игнорировать пробелы и комментарии
+        (?:[^<>\^]?[<>\^])? # [[fill]align], Незахватывающая группа,
+                            # [fill] > [^<>\^]? любой символ кроме <>^ символ не обязательный
+                            # align > [<>\^] любой из символов <>^ ,символа может не быть
+        (?P<sign>[\+-])?   # [sign] > [+-] Именованная группа sign, символ + или - ,символа может не быть
+        \#?                 # ['#'] Флаг, захват не нужен
+        0?                  # ['0'] Флаг, захват не нужен
+        (?P<width>\d+)?     # [width]  Ширина
+        (?:\.(?P<precision>(\*|(?P<raw_width>\d+)(?P<end_width>\$)?)))?    # ['.' precision]   Точность
+            "
+        )
+        .unwrap()
+    }) ;
+
+    let mut sign_out = Option::<Sign>::None ;
+    let mut width_out= Option::<usize>::None ;
+    let mut precision_out = Option::<Precision>::None ;
+
+    // прощедура ищет первое совпадение с данным регулярным выражением
+    if let Some(data) = re.captures(input) {
+        // Возвращает объект Match, связанный с группой захвата с именем "sign".
+        if let Some(m) = data.name("sign") {
+           sign_out = match m.as_str() {
+               "+" => Some(Sign::Plus),
+               "-" => Some(Sign::Minus),
+               _ => Option::<Sign>::None,
+           } ;
+        }
+
+        // Возвращает объект Match, связанный с группой захвата с именем "width".
+        if let Some(m) = data.name("width") {
+            width_out = match m.as_str().parse::<usize>() {
+             Ok(w)   => Some(w),
+             Err(err) => panic!("cannot convert {} to usize, error: {}", m.as_str(), err),
+            }
+        }
+
+        // Возвращает объект Match, связанный с группой захвата с именем "precision".
+        if let Some(m) = data.name("precision") {
+            precision_out = match m.as_str() {
+               "*" => Some(Precision::Asterisk),
+               _ => {
+                    // получаем raw width
+                    let w = match data.name("raw_width") {
+                        Some(m) => {
+                            match m.as_str().parse::<usize>() {
+                                Ok(w) => w,
+                                Err(err) => panic!("Invalid raw_width: {}, error: {}", m.as_str(), err),
+                            }
+                        },
+                        None => panic!("Not found raw_width"),
+                    } ;
+
+                    // определеяем width
+                    match data.name("end_width") {
+                      Some(_) => Some(Precision::Argument(w)),
+                      None => Some(Precision::Integer(w)),
+                    }
+                }
+            } ;
+        }
+    }
+
+    (sign_out, width_out, precision_out)
 }
 
 #[derive(Debug, PartialEq)]
