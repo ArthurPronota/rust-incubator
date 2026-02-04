7382d68e16af35d8ebb52508c75849afa3b744f3
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-02-04 08:36:09 +0500

    Update main.rs

--- 3_ecosystem/3_4_regex_parsing/src/main.rs
+++ 3_ecosystem/3_4_regex_parsing/src/main.rs
@@ -26,10 +26,32 @@ fn main() {
             ) ;
     println!("res: {:?}", res) ;
 
-    {
-        use winnow::Result;
+    {   // Глава 1:
 
-        
+        // pub type Result<O, E = ContextError> = core::result::Result<O, E>
+        use winnow::Result;
+        // корневой trait для parsing
+        use winnow::Parser ;
+
+        pub fn do_nothing_parser<'s>(input: &mut &'s str) -> Result<&'s str> {
+            Ok("")
+        }
+
+        let mut input = "0x1a2b Hello";
+
+        let output = 
+                do_nothing_parser
+        /*
+        Извлекать токены из потока, преобразуя их в выходные данные.
+        Это включает в себя продвижение входного потока к следующему местоположению.
+        В случае ошибки входные данные будут указывать на место ошибки.                    
+        */
+                    .parse_next(&mut input)
+                    .unwrap()
+                    ;
+
+        assert_eq!(input, "0x1a2b Hello");
+        assert_eq!(output, "");
     }
 }
 
