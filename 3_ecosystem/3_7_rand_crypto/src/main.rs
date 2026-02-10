06a3ce6e9e8bbc0e16b4556126cbd117211af054
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-02-10 11:41:52 +0500

    3.7

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -1,4 +1,8 @@
-use rand::Rng ;
+
+use rand::{
+        // Это основной трейт, используемый при генерации случайных значений.
+        Rng,
+    } ;
 
 /*
 Добавить в Cargo.toml именно эту версию rand
@@ -10,6 +14,7 @@ use rand::{
     // криптографически стойкий.    
     thread_rng
 } ;
+use sha3::Digest;
 
 /// Генерация пароля указанной длины из заданного charset
 fn generate_password(
@@ -34,6 +39,7 @@ fn generate_password(
         },
     }
 
+    /* так:
     Ok(
         // формирование пароля
         (0..pass_len)
@@ -45,6 +51,38 @@ fn generate_password(
             })
             .collect::<String>()
     ) 
+     */
+
+    // или так:
+    // Генератор случайных чисел, который можно явно задать в качестве 
+    // начального значения.
+    use rand::SeedableRng ;
+
+    /*
+    Добавить в Cargo.toml именно эту версию rand
+    [dependencies]
+    rand = { version ="0.8.5",  features = ["small_rng"] }
+     */
+    // Быстрый некриптографический генератор псевдослучайных чисел с 
+    // небольшим объемом памяти    
+    use rand::rngs::SmallRng ;
+
+    // Создает новый экземпляр генератора случайных чисел, начальное 
+    // значение которого задается с помощью функции getrandom.
+    // getrandom — это библиотека Rust для получения случайных данных из 
+    // источников (операционной) системы: https://docs.rs/getrandom/latest/getrandom/
+    let mut s_rng = SmallRng::from_entropy() ;
+
+    Ok(
+        (0..pass_len)
+            .map(|_|
+            vec_charset[
+                s_rng.gen_range(0..vec_charset.len()) 
+            ]  
+            )
+            .collect()
+    )
+
 }
 
 /// Извлекает случайный элемент из заданного среза
@@ -54,10 +92,123 @@ fn select_rand_val<T>(slice: &[T]) ->Result<&T, String> {
         return Err("slice is empty".to_string()) ;
     }
 
+    /* так:
     Ok(&slice[
             thread_rng()
                 .gen_range(0..slice.len())
             ]
+    )
+     */
+    
+    // или так:
+    use rand::{
+            // Генератор случайных чисел, который можно явно задать в качестве 
+            // начального значения.
+            SeedableRng
+        };
+
+    /*
+    Добавить в Cargo.toml именно эту версию rand
+    [dependencies]
+    rand = { version ="0.8.5",  features = ["small_rng"] }
+     */
+    // Быстрый некриптографический генератор псевдослучайных чисел с 
+    // небольшим объемом памяти
+    use rand::rngs::SmallRng;
+
+    Ok(
+        &slice[
+            // SmallRng::from_entropy() - Создает новый экземпляр генератора 
+            // случайных чисел, инициализируемый с помощью функции getrandom.
+            // getrandom — это библиотека Rust для получения случайных данных из 
+            // источников (операционной) системы: https://docs.rs/getrandom/latest/getrandom/
+            SmallRng::from_entropy()    
+                .gen_range(0..slice.len())
+        ]
+    )
+
+}
+
+/// генерирует уникальное криптографически безопасное случайное значение 
+/// в наборе символов `a-zA-Z0-9` и содержит ровно `64` символа
+fn new_access_token() ->String {
+
+    let charset: &[u8] = 
+            b"abcdefghijklmnopqrstuvwxyz\
+             ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+             0123456789" ;
+
+    (0..64)
+        .map(|_| 
+            charset[
+                thread_rng()
+                    .gen_range(0..charset.len())
+                ] as char
+        )
+        .collect()
+}
+
+/// возвращает хеш SHA-3 файла, указанного по его пути.
+fn get_file_hash(path: &str) ->Result<String, String>{
+    // проверка что path не пуст
+    if path.is_empty() {
+        return Err("path is empty".to_string());
+    }
+
+    // Объект, предоставляющий доступ к открытому файлу в файловой системе.
+    use std::fs::File ;
+
+    use std::io::{
+            BufReader,  // Структура BufReader<R> добавляет буферизацию к любому считывателю.
+            Read        // Трейт Read позволяет считывать байты из источника.
+        } ;
+    // Реализация криптографического алгоритма хеширования SHA-3.
+    use sha3::Sha3_256 ;
+
+    // Определение Reader
+    let file ;
+    // Попытка открыть файл в режиме только для чтения.
+    match File::open(path) {
+        Ok(v) => file = v,
+        Err(err) => return Err(err.to_string()),
+    } 
+
+    // Создает новый объект BufReader<R> с емкостью буфера по умолчанию (8 KiB).
+    let mut reader = BufReader::new(file) ;
+
+    // Определение буфера для чтения из файла
+    let mut buffer = [0; 8192] ;
+
+    // Создать новый экземпляр hasher.
+    // Это инкрементальный хешер, который может обрабатывать данные по частям
+    let mut hasher = Sha3_256::new() ;
+
+    loop {
+        let readed_bytes ;
+        match reader.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(v) => readed_bytes = v,
+            Err(err) => return Err(err.to_string()),
+        }
+
+        // Обработка данных, обновление внутреннего состояния.
+        // Принимает срез байтов (&[u8]) - любые данные
+        // Обновляет внутреннее состояние хешера
+        // Может вызываться много раз для одного хеша
+        hasher.update(&buffer[..readed_bytes]);
+    }
+    
+    /*
+        hasher.finalize() - это финализирующий метод, который завершает 
+            процесс хеширования и возвращает окончательный хеш.
+    1. Добавляет padding (дополнение) к оставшимся данным
+    2. Выполняет финальный раунд преобразований Keccak
+    3. Извлекает итоговый хеш из внутреннего состояния
+    4. Сбрасывает/потребляет хешер (после finalize() хешер нельзя использовать)
+     */
+
+    Ok(
+        format!("{:x}", hasher.finalize())
     )
 }
 
@@ -79,4 +230,8 @@ fn main() {
     println!("{:?}", select_rand_val(&["abc", "def", "gkj"])) ;
 
     println!("{:?}", select_rand_val(&[(1, 2), (3, 4), (5, 6)])) ;
+
+    println!("access_token: {}", new_access_token()) ;
+
+    println!("{:?}", get_file_hash("Cargo.toml"))
 }
