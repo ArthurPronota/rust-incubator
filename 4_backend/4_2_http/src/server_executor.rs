3b210a2ad273fe1791c203233c757f7a894c20d4
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-03-18 17:07:51 +0500

    Update server_executor.rs

--- 4_backend/4_2_http/src/server_executor.rs
+++ 4_backend/4_2_http/src/server_executor.rs
@@ -1,5 +1,4 @@
 use anyhow::{
-        //Ok,
         Result
     } ;
 
@@ -151,16 +150,6 @@ async fn handle_commmand_int(
                     let mut list_ur = vec![] ;
 
                     for id_user in users::User::get_all_id_user(&mut *trans).await? {
-                        /*
-                        let ur = 
-                            users::UserWithRole::get_data(
-                                &mut *trans,
-                                id_user
-                            )
-                            .await? ;
-                        print!("{}", ur) ;  // печать пользователя
-                        println!("--------------------------------------------") ;
-                         */
                         list_ur.push(
                             users::UserWithRole::get_data(
                                 &mut *trans,
@@ -169,7 +158,6 @@ async fn handle_commmand_int(
                             .await?
                         );
                     }
-                    //Ok(local_success("User's email changed successfully."))                                    
                     Ok(common::Response::ListUsersRoles(list_ur))
                 }
             }
@@ -248,17 +236,6 @@ async fn handle_commmand_int(
             // Проверка кода роли
             match slug {
                 Some(slug) => { // код роли найден, печать этой роли
-                    /*
-                    println!(
-                        "{}", 
-                        roles::Role::find_slug_raise(
-                                &mut *trans,
-                                slug,
-                                false
-                            )
-                            .await?                
-                    ) ;
-                    */
                     Ok(common::Response::Role( 
                         roles::Role::find_slug_raise(
                                 &mut *trans,
@@ -272,18 +249,6 @@ async fn handle_commmand_int(
                     let mut list_roles = vec![] ;
 
                     for sl in roles::Role::get_all_slugs(&mut *&mut trans).await? {
-                        /*
-                        print!(
-                            "{}", 
-                            roles::Role::find_slug_raise(
-                                    &mut *trans,
-                                    &sl,
-                                    false
-                            )
-                            .await?                
-                        ) ;
-                        println!("--------------------------------------------") ;
-                         */
                         list_roles.push(
                             roles::Role::find_slug_raise(
                                     &mut *trans,
@@ -346,73 +311,11 @@ pub async fn handle_commmand(
                     Json(cmd): Json<args::Commands>
                 ) 
                 ->  JsonResponse<common::Response> 
-                //Json<common::Response>
 {
     // Внутренная обработка команд
     match handle_commmand_int(&db_res, &cmd).await {
         Ok(v) => JsonResponse(v),
         Err(err) => JsonResponse(common::Response::Error(err.to_string())),
     } 
-
-    /*
-    match cmd {
-        Commands::InitDb => {
-
-            match db_res.create_tables().await {
-                Ok(_) => JsonResponse(common::Response::Success("Database objects created successfully.".to_owned())),
-                Err(err) => JsonResponse(common::Response::Error(err.to_string())),
-            }
-            //JsonResponse(common::Response::Success("Database objects created successfully.".to_owned()))
-            //Json(common::Response::Success("Database objects created successfully.".to_owned()))
-        },
-        // Создание пользователя        
-        Commands::CreateUser { name, email } => {
-            // Сформировать новую транзакцию
-            let mut trans = 
-                    match 
-                        db_res
-                            .pool
-                            // Устанавливает соединение и немедленно начинает новую транзакцию.
-                            .begin()
-                            .await 
-                    {
-                      Ok(v) => v,
-                      Err(err) => return JsonResponse(common::Response::Error(err.to_string())),
-                    } ;
-
-            // вставить нового пользователя
-            let new_user = 
-                    match users::User::ins_user(
-                            &mut *trans,
-                            &name,
-                            &email
-                          )
-                          .await {
-                        Ok(v) => v,
-                        Err(err) => return JsonResponse(common::Response::Error(err.to_string())),
-                    } ;
-            
-            // Добавить для пользователя роль по умолчанию
-            if let Err(err) = users_roles::UsersRoles::ins_role_to_user(
-                            &mut *trans,
-                            new_user.id_user(),
-                            roles::SLUG_DEFAULT
-                        )
-                        .await
-            {
-                return JsonResponse(common::Response::Error(err.to_string())) ;
-            } 
-
-            // выполнить commit в DB
-            if let Err(err) = trans.commit().await {
-                return JsonResponse(common::Response::Error(err.to_string())) ;
-            }
-
-            // println!("User created successfully.") ;                
-            JsonResponse(common::Response::Success("User created successfully.".to_owned()))
-        },
-
-    }
-    */
 }
 
