7bbc6c40848cfbfc4727d2acce649590346b39c6
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-04-01 10:09:09 +0500

    4.3

--- 4_backend/src/bin/server.rs
+++ 4_backend/src/bin/server.rs
@@ -9,6 +9,7 @@ CREATE DATABASE `4_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
 
 */
 use anyhow::Result ;
+use sqlx::migrate;
 
 use std::sync::Arc ;
 
@@ -107,6 +108,59 @@ async fn main() ->Result<()> {
     // Создать таблицы в DB      
     db_res.create_tables()
         .await? ;
+
+    /* Выполнение миграций
+    При выполнении миграции создаётся таблица _sqlx_migrations.
+
+ MySQL  localhost:3306 ssl  4_db  SQL > desc _sqlx_migrations ;
++----------------+------------+------+-----+-------------------+-------------------+
+| Field          | Type       | Null | Key | Default           | Extra             |
++----------------+------------+------+-----+-------------------+-------------------+
+| version        | bigint     | NO   | PRI | NULL              |                   |
+| description    | text       | NO   |     | NULL              |                   |
+| installed_on   | timestamp  | NO   |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
+| success        | tinyint(1) | NO   |     | NULL              |                   |
+| checksum       | blob       | NO   |     | NULL              |                   |
+| execution_time | bigint     | NO   |     | NULL              |                   |
++----------------+------------+------+-----+-------------------+-------------------+
+6 rows in set (0.0027 sec)
+ MySQL  localhost:3306 ssl  4_db  SQL >    
+
+ MySQL  localhost:3306 ssl  4_db  SQL > select * from _sqlx_migrations \G
+*************************** 1. row ***************************
+       version: 20260401000000
+   description: initial
+  installed_on: 2026-04-01 10:04:35
+       success: 1
+      checksum: 0x81997E09F9A43D55633C5C258C868DD64F173FC204F03CC973AE5B8456DD8816A76B06AEAFC4EF29ACF8B6ED61657959
+execution_time: 18625100
+*************************** 2. row ***************************
+       version: 20260401000001
+   description: two
+  installed_on: 2026-04-01 10:04:35
+       success: 1
+      checksum: 0xEC4064DDFCC9878E3AA294AEA757BE0D10A5938B5E7C6134A35E7AB5B643CCC73C8C04CE2ABF705CF73AAA26E8426E5D
+execution_time: 19653300
+2 rows in set (0.0007 sec)
+ MySQL  localhost:3306 ssl  4_db  SQL >
+
+    Формат имени файла: migrations\20260401000001_two.sql
+        20260401000001 - YYYYmmddHHmiss (version)
+        two - description
+
+    Если в файле миграции одно sql предложени то не нужно ставить ';'
+    Если в файле миграции несколько sql предложения то ';' ставится 
+        после всех sql предложений.
+
+    Код ниже:
+    1. Находит все миграции в папке migrations/
+    2. Проверяет, какие миграции уже выполнены
+    3. Выполняет новые миграции в правильном порядке
+    4. Создает и обновляет таблицу _sqlx_migrations для отслеживания состояния
+
+     */
+
+    sqlx::migrate!().run(&db_res.pool).await?;
 
     let schema = 
                     Schema::build(
